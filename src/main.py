a83b80746a888b3f1059804f12ec3e760740b096
wrogers465 <wrogers465@gmail.com>
2023-04-07 15:37:15 +0000

    Added basic tkinter window w/ ice email btn

--- src/main.py
+++ src/main.py
@@ -1,5 +1,24 @@
+import tkinter as tk
+
+
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Work Assistant")
+
+        #EMAIL COMPONENTS FRAME
+        email_frm = tk.Frame(self)
+        ice_email_btn = tk.Button(email_frm, text="ICE Email", command=None)
+
+        #EMAIL COMPONENTS PLACEMENT
+        email_frm.grid()
+        ice_email_btn.grid()
+
+
 def main():
-    pass
+    root = MainWindow()
+    root.mainloop()
 
 
 if __name__ == '__main__':
